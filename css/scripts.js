b54290a29ddafb4053432348a179afd4ba9c031a
wasongapaul5 <wasongapaul5@gmail.com>
2019-09-23 12:08:49 +0300

    added other functions

--- css/scripts.js
+++ css/scripts.js
@@ -7,3 +7,30 @@ function akanName() {
       MM = d.getMonth();
       CC = d.getDate();
   
+    }else {/* Invalid date */
+        alert("Invalid date");                                                                                                                                             
+      }
+ var male =  ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+ var female = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+ var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+
+ var dayCalculate = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10))) % 7;
+
+ var gender = document.getElementById('genders').value;
+ console.log(dayCalculate);
+ if (document.getElementById('male').checked) {
+   gender = document.getElementById('male').value;
+   alert(" Since you were born on " + dayOfTheWeek[dayCalculate] + ',' + " your name is " + male[dayCalculate]);
+   document.getElementById('result').innerHTML = male[dayCalculate];
+
+ }
+ if (document.getElementById('female').checked) {
+   gender = document.getElementById('female').value;
+   alert(" Since you were born on " + dayOfTheWeek[dayCalculate] + ',' + " your name is " + female[dayCalculate]);
+   document.getElementById('result').innerHTML = female[dayCalculate];
+
+ }else {
+   alert("choose your gender");
+ }
+
+};
